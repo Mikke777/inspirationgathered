755d725bed454f19612aea38eb1204042ac3ccd6
michele di stadio <distadiomichele93@gmail.com>
2025-01-06 18:07:16 +0100

    description added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_06_155319) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_06_165029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_06_155319) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "last_name"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
